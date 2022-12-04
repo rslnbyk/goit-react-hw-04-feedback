2b7f476656c525e0a6f417ab88ce343433ac5978
rslnbyk <74673598+rslnbyk@users.noreply.github.com>
2022-12-04 17:06:31 +0200

    small fixes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,13 +17,13 @@ export const App = () => {
   const leaveFeedback = type => {
     switch (type) {
       case 'good':
-        setGood(good + 1);
+        setGood(prev => prev + 1);
         break;
       case 'neutral':
-        setNeutral(neutral + 1);
+        setNeutral(prev => prev + 1);
         break;
       case 'bad':
-        setBad(bad + 1);
+        setBad(prev => prev + 1);
         break;
       default:
         return;
